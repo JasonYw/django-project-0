d132e5ce062bc16443add3dabf535ba02295c6ca
rico_mac <jasonwu@outlook.com>
2020-11-25 22:03:10 +0800

    1

--- project_0/project_0/settings.py
+++ project_0/project_0/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'project_0.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,TEMPLATES)],
+        'DIRS': [os.path.join(BASE_DIR,"TEMPLATES")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
