00bd2d32f7a59da887726183ad9597ed1fb9e368
rico <jasonwu@outlook.com>
2020-11-28 15:28:16 +0800

    1

--- project_0/app01/views.py
+++ project_0/app01/views.py
@@ -1,25 +1,49 @@
 import sys
-import os 
+import os
+import json
 import random
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
 from app01 import models
-from app01.models import RegisterModelForm
+from app01.models import RegisterModelForm, UserSms
+
 project_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 sys.path.append(project_dir)
-from utils.tencent.sms import LoginSMS,ReginterSMS,ResetPasswordSMS
+from utils.tencent.sms import LoginSMS, ReginterSMS, ResetPasswordSMS
 
 # Create your views here.
 
-def send_sms(request):
-    '''
-    发送短信
-    '''
-    code =random.randrange(1000,9999)
-    res =LoginSMS.send_sms_single('15801367721',code)
-    if res.get('result',None) ==0:
-        return HttpResponse('200')
 
-    
+def send_sms(request):
+    """
+    发送短信
+    """
+    code = random.randrange(1000, 9999)
+    phonenumber = request.POST.get("phonenumber")
+    functioncode = request.POST.get("functioncode")
+
+    if functioncode == "login":
+        resobject = LoginSMS
+    if functioncode == "register":
+        resobject = ReginterSMS
+    if functioncode == "resetpassword":
+        resobject = ResetPasswordSMS
+    # res =resobject.send_sms_single(phonenumber,code)
+    res = {"result": 0}
+    if res.get("result", None) == 0:
+        UserSms.record_register(phonenumber, code, functioncode)
+        return HttpResponse("200")
+    else:
+        return HttpResponse("请稍后重试")
+
+
 def register(request):
-    form =RegisterModelForm()
-    return render(request,'register.html',{'form':form})
+    if request.method == "GET":
+        form = RegisterModelForm()
+        return render(request, "register.html", {"form": form})
+    if request.method == "POST":
+        obj = RegisterModelForm(request.POST)
+        if obj.is_valid():
+            print(obj.cleaned_data)
+        else:
+            print(obj.errors)
+            return HttpResponse(json.dumps({"message": obj.errors}))
--- project_0/utils/tencent/sms.py
+++ project_0/utils/tencent/sms.py
@@ -5,24 +5,30 @@ import sys
 import os
 from qcloudsms_py import SmsMultiSender, SmsSingleSender
 from qcloudsms_py.httpclient import HTTPError
-project_dir =os.path.abspath(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+
+project_dir = os.path.abspath(
+    os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+)
 sys.path.append(project_dir)
 from project_0 import settings
+
 # ssl._create_default_https_context = ssl._create_unverified_context
 
 
-APPKEY =settings.APPKEY
-SMSSIGH =settings.SMSSIGN
-SSDAPPID =settings.SSDAPPID
-LOGINID =settings.LOGINID
-REGISTERID =settings.REGISTERID
-RESETPASSWORDID =settings.RESETPASSWORDID
+APPKEY = settings.APPKEY
+SMSSIGH = settings.SMSSIGN
+SSDAPPID = settings.SSDAPPID
+LOGINID = settings.LOGINID
+REGISTERID = settings.REGISTERID
+RESETPASSWORDID = settings.RESETPASSWORDID
+
+
 class BaseSMS:
-    APPID =""
-    EXPIRES =5
+    APPID = ""
+    EXPIRES = 10
 
     @classmethod
-    def send_sms_single(cls,phone_num,randomcode):
+    def send_sms_single(cls, phone_num, randomcode):
         """
         单条发送短信
         :param  手机号
@@ -31,25 +37,30 @@ class BaseSMS:
         :return:
         """
         if cls.EXPIRES != None:
-            param_list =[randomcode,cls.EXPIRES]
+            param_list = [randomcode, cls.EXPIRES]
         else:
-            param_list =[randomcode,]
+            param_list = [randomcode]
         sender = SmsSingleSender(SSDAPPID, APPKEY)
         try:
-            response = sender.send_with_param(86, phone_num,cls.APPID,param_list,sign=SMSSIGH)
+            response = sender.send_with_param(
+                86, phone_num, cls.APPID, param_list, sign=SMSSIGH
+            )
         except HTTPError as e:
-            response = {'result': 1000, 'errmsg': "网络异常发送失败"}
+            response = {"result": 1000, "errmsg": "网络异常发送失败"}
         return response
 
+
 class LoginSMS(BaseSMS):
-    APPID =LOGINID
+    APPID = LOGINID
+
 
 class ReginterSMS(BaseSMS):
-    APPID =REGISTERID
+    APPID = REGISTERID
+
 
 class ResetPasswordSMS(BaseSMS):
-    APPID =RESETPASSWORDID
-    EXPIRES =None
+    APPID = RESETPASSWORDID
+    EXPIRES = None
 
 
 # def send_sms_multi(phone_num_list, template_id, param_list):
